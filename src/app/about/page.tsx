f905e5cca794188b843e4ede113e86bfe668bdb4
Zach Cygan <zachcygan@gmail.com>
2024-10-22 19:30:22 -0600

    padding changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,14 +9,14 @@ const switzer = localFont({
 
 export default function About() {
   return (
-    <div className="py-10 lg:py-20 lg:pb-80 bg-darkGreen h-full">
+    <div className="py-10 lg:py-20 lg:pb-[25.5rem] bg-darkGreen h-full">
       <div className={`text-center text-white ${switzer.className}`}>
-        <div className={`font-bold text-6xl pt-24`}>
+        <div className={`font-bold text-6xl pt-32 pb-2`}>
           About us
         </div>
-        <div className="pt-10 px-4 lg:px-0 text-xl lg:text-3xl max-w-[44ch] mx-auto space-y-10 text-start">
+        <div className="pt-10 px-4 pb-28 lg:px-0 text-lg/7 md:text-3xl max-w-[44ch] mx-auto space-y-10 text-start">
           <div>
-            PL8CHAT is a technology transforming license
+            PL8CHAT is a technology company transforming license
             plates into the universal line of vehicle communication.
           </div>
           <div>
