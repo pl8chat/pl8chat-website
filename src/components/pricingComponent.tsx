14c7942c31f95bce68b385d00c3068c1b224768c
Zach <vercel.clinic576@slmails.com>
2024-11-07 20:28:07 -0700

    pricing component changes

--- src/components/pricingComponent.tsx
+++ src/components/pricingComponent.tsx
@@ -14,32 +14,27 @@ type Tier = {
   name: string
   id: string
   href: string
-  price: { monthly: string; annually: string }
+  price: string
   description: string
   features: string[]
   mostPopular: boolean
 }
 
-const frequencies: Frequency[] = [
-  { value: 'monthly', label: 'Monthly', priceSuffix: '/month' },
-  { value: 'annually', label: 'Annually', priceSuffix: '/year' },
-]
-
 const tiers: Tier[] = [
   {
-    name: 'Freelancer',
+    name: 'Standard',
     id: 'tier-freelancer',
     href: '#',
-    price: { monthly: '$19', annually: '$199' },
+    price: 'Free',
     description: 'The essentials to provide your best work for clients.',
     features: ['5 products', 'Up to 1,000 subscribers', 'Basic analytics', '48-hour support response time'],
-    mostPopular: false,
+    mostPopular: true,
   },
   {
-    name: 'Startup',
+    name: 'Premium',
     id: 'tier-startup',
     href: '#',
-    price: { monthly: '$29', annually: '$299' },
+    price: '$299',
     description: 'A plan that scales with your rapidly growing business.',
     features: [
       '25 products',
@@ -48,13 +43,13 @@ const tiers: Tier[] = [
       '24-hour support response time',
       'Marketing automations',
     ],
-    mostPopular: true,
+    mostPopular: false,
   },
   {
     name: 'Enterprise',
     id: 'tier-enterprise',
     href: '#',
-    price: { monthly: '$59', annually: '$599' },
+    price: '$599',
     description: 'Dedicated support and infrastructure for your company.',
     features: [
       'Unlimited products',
@@ -73,7 +68,8 @@ function classNames(...classes: string[]) {
 }
 
 export default function PricingComponent() {
-  const [frequency, setFrequency] = useState<Frequency>(frequencies[0])
+  const mostPopularTierId = tiers.find((tier) => tier.mostPopular)?.id || null // Get ID of the most popular tier
+  const [selectedTierId, setSelectedTierId] = useState<string | null>(mostPopularTierId) // Set initial state to most popular
 
   return (
     <div className="bg-lightGrey py-24 sm:py-32">
@@ -87,32 +83,15 @@ export default function PricingComponent() {
           Choose an affordable plan that’s packed with the best features for engaging your audience, creating customer
           loyalty, and driving sales.
         </p>
-        <div className="mt-16 flex justify-center">
-          <fieldset aria-label="Payment frequency">
-            <RadioGroup
-              value={frequency}
-              onChange={setFrequency}
-              className="grid grid-cols-2 gap-x-1 rounded-full p-1 text-center text-xs/5 font-semibold ring-1 ring-inset ring-gray-200"
-            >
-              {frequencies.map((option) => (
-                <Radio
-                  key={option.value}
-                  value={option}
-                  className="cursor-pointer rounded-full px-2.5 py-1 text-gray-500 data-[checked]:bg-darkGreen data-[checked]:text-white"
-                >
-                  {option.label}
-                </Radio>
-              ))}
-            </RadioGroup>
-          </fieldset>
-        </div>
-        <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+
+        <div className="isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {tiers.map((tier) => (
             <div
               key={tier.id}
+              onClick={() => setSelectedTierId(tier.id)} // Set selected tier ID on click
               className={classNames(
-                tier.mostPopular ? 'ring-2 ring-darkGreen' : 'ring-1 ring-gray-200',
-                'rounded-3xl p-8 xl:p-10',
+                selectedTierId === tier.id ? 'ring-2 ring-darkGreen' : 'ring-1 ring-gray-200', // Apply green outline if the tier is selected
+                'rounded-3xl p-8 xl:p-10 cursor-pointer',
               )}
             >
               <div className="flex items-center justify-between gap-x-4">
@@ -132,11 +111,18 @@ export default function PricingComponent() {
                 ) : null}
               </div>
               <p className="mt-4 text-sm/6 text-gray-600">{tier.description}</p>
-              <p className="mt-6 flex items-baseline gap-x-1">
+              <p className="mt-6 flex items-baseline gap-x-1 relative">
                 <span className="text-4xl font-semibold tracking-tight text-gray-900">
-                  {tier.price[frequency.value]}
+                  {tier.price}
                 </span>
-                <span className="text-sm/6 font-semibold text-gray-600">{frequency.priceSuffix}</span>
+                {tier.price == 'Free' ? 
+                <div className=''>
+                  <span className="text-sm/6 font-semibold text-gray-600">/first year</span>
+                  <span className='text-sm/6 font-normal text-gray-600 absolute left-0 -bottom-4'>$50/year</span>
+                </div>
+                  :
+                  <span className="text-sm/6 font-semibold text-gray-600">/year</span>
+              }
               </p>
               <a
                 href={tier.href}
@@ -148,7 +134,15 @@ export default function PricingComponent() {
                   'mt-6 block rounded-md px-3 py-2 text-center text-sm/6 font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-darkGreen',
                 )}
               >
-                Buy plan
+                {tier.name == 'Enterprise' ?
+                  <div>
+                    Contact Sales
+                  </div>
+                  :
+                  <div>
+                    Buy plan
+                  </div>
+                }
               </a>
               <ul role="list" className="mt-8 space-y-3 text-sm/6 text-gray-600 xl:mt-10">
                 {tier.features.map((feature) => (
