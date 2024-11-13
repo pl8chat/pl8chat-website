7afd2a473f0ddf5e13644b7ed8e0e708f68291ef
zachcygan <zachcygan@gmail.com>
2024-11-12 21:09:58 -0700

    free sign up flow complete

--- src/components/freePlanForm.tsx
+++ src/components/freePlanForm.tsx
@@ -262,73 +262,97 @@ export default function FreePlanForm() {
                 <div onClick={handleNext} className="w-[460px] cursor-pointer"><span className="text-gray-600 text-sm font-normal leading-tight">Don’t have a physical address? </span><span className="text-[#00695c] text-sm font-normal underline leading-tight">Skip this step</span></div>
               </div>
               <div className="w-[460px] h-[336px] flex-col justify-start items-start gap-6 inline-flex">
-                <Input variant='default' label='Address line 1 (Street address or post office box)' id='firstName' name='firstName' type='text' />
-                <Input variant='default' label='Address line 2' id='firstName' name='firstName' type='text' />
-
+                <Input variant='default' label='Address line 1 (Street address or post office box)' id='address1' name='address1' type='text' />
+                <Input variant='default' label='Address line 2' id='address2' name='address2' type='text' />
                 <div className="self-stretch justify-start items-start gap-2 inline-flex">
-                  <div className="self-stretch justify-start items-start gap-2 inline-flex">
-                    <Input variant='default' label='City' id='firstName' name='firstName' type='text' />
-                    <div className='w-full'>
-                      <Select>
-                        <SelectTrigger label='State'>
-                          <SelectValue placeholder="Select one" />
-                        </SelectTrigger>
-                        <SelectContent>
-                          <SelectItem value="AL">Alabama</SelectItem>
-                          <SelectItem value="AK">Alaska</SelectItem>
-                          <SelectItem value="AZ">Arizona</SelectItem>
-                          <SelectItem value="AR">Arkansas</SelectItem>
-                          <SelectItem value="CA">California</SelectItem>
-                          <SelectItem value="CO">Colorado</SelectItem>
-                          <SelectItem value="CT">Connecticut</SelectItem>
-                          <SelectItem value="DE">Delaware</SelectItem>
-                          <SelectItem value="FL">Florida</SelectItem>
-                          <SelectItem value="GA">Georgia</SelectItem>
-                          <SelectItem value="HI">Hawaii</SelectItem>
-                          <SelectItem value="ID">Idaho</SelectItem>
-                          <SelectItem value="IL">Illinois</SelectItem>
-                          <SelectItem value="IN">Indiana</SelectItem>
-                          <SelectItem value="IA">Iowa</SelectItem>
-                          <SelectItem value="KS">Kansas</SelectItem>
-                          <SelectItem value="KY">Kentucky</SelectItem>
-                          <SelectItem value="LA">Louisiana</SelectItem>
-                          <SelectItem value="ME">Maine</SelectItem>
-                          <SelectItem value="MD">Maryland</SelectItem>
-                          <SelectItem value="MA">Massachusetts</SelectItem>
-                          <SelectItem value="MI">Michigan</SelectItem>
-                          <SelectItem value="MN">Minnesota</SelectItem>
-                          <SelectItem value="MS">Mississippi</SelectItem>
-                          <SelectItem value="MO">Missouri</SelectItem>
-                          <SelectItem value="MT">Montana</SelectItem>
-                          <SelectItem value="NE">Nebraska</SelectItem>
-                          <SelectItem value="NV">Nevada</SelectItem>
-                          <SelectItem value="NH">New Hampshire</SelectItem>
-                          <SelectItem value="NJ">New Jersey</SelectItem>
-                          <SelectItem value="NM">New Mexico</SelectItem>
-                          <SelectItem value="NY">New York</SelectItem>
-                          <SelectItem value="NC">North Carolina</SelectItem>
-                          <SelectItem value="ND">North Dakota</SelectItem>
-                          <SelectItem value="OH">Ohio</SelectItem>
-                          <SelectItem value="OK">Oklahoma</SelectItem>
-                          <SelectItem value="OR">Oregon</SelectItem>
-                          <SelectItem value="PA">Pennsylvania</SelectItem>
-                          <SelectItem value="RI">Rhode Island</SelectItem>
-                          <SelectItem value="SC">South Carolina</SelectItem>
-                          <SelectItem value="SD">South Dakota</SelectItem>
-                          <SelectItem value="TN">Tennessee</SelectItem>
-                          <SelectItem value="TX">Texas</SelectItem>
-                          <SelectItem value="UT">Utah</SelectItem>
-                          <SelectItem value="VT">Vermont</SelectItem>
-                          <SelectItem value="VA">Virginia</SelectItem>
-                          <SelectItem value="WA">Washington</SelectItem>
-                          <SelectItem value="WV">West Virginia</SelectItem>
-                          <SelectItem value="WI">Wisconsin</SelectItem>
-                          <SelectItem value="WY">Wyoming</SelectItem>
-                        </SelectContent>
-                      </Select>
-                    </div>
+                  <Input
+                    variant='default'
+                    label='City'
+                    id='firstName'
+                    name='firstName'
+                    type='text'
+                    className='flex-1' />
+                  <div className='flex-1'>
+                    <Select>
+                      <SelectTrigger label='State'>
+                        <SelectValue placeholder="Select one" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="AL">Alabama</SelectItem>
+                        <SelectItem value="AK">Alaska</SelectItem>
+                        <SelectItem value="AZ">Arizona</SelectItem>
+                        <SelectItem value="AR">Arkansas</SelectItem>
+                        <SelectItem value="CA">California</SelectItem>
+                        <SelectItem value="CO">Colorado</SelectItem>
+                        <SelectItem value="CT">Connecticut</SelectItem>
+                        <SelectItem value="DE">Delaware</SelectItem>
+                        <SelectItem value="FL">Florida</SelectItem>
+                        <SelectItem value="GA">Georgia</SelectItem>
+                        <SelectItem value="HI">Hawaii</SelectItem>
+                        <SelectItem value="ID">Idaho</SelectItem>
+                        <SelectItem value="IL">Illinois</SelectItem>
+                        <SelectItem value="IN">Indiana</SelectItem>
+                        <SelectItem value="IA">Iowa</SelectItem>
+                        <SelectItem value="KS">Kansas</SelectItem>
+                        <SelectItem value="KY">Kentucky</SelectItem>
+                        <SelectItem value="LA">Louisiana</SelectItem>
+                        <SelectItem value="ME">Maine</SelectItem>
+                        <SelectItem value="MD">Maryland</SelectItem>
+                        <SelectItem value="MA">Massachusetts</SelectItem>
+                        <SelectItem value="MI">Michigan</SelectItem>
+                        <SelectItem value="MN">Minnesota</SelectItem>
+                        <SelectItem value="MS">Mississippi</SelectItem>
+                        <SelectItem value="MO">Missouri</SelectItem>
+                        <SelectItem value="MT">Montana</SelectItem>
+                        <SelectItem value="NE">Nebraska</SelectItem>
+                        <SelectItem value="NV">Nevada</SelectItem>
+                        <SelectItem value="NH">New Hampshire</SelectItem>
+                        <SelectItem value="NJ">New Jersey</SelectItem>
+                        <SelectItem value="NM">New Mexico</SelectItem>
+                        <SelectItem value="NY">New York</SelectItem>
+                        <SelectItem value="NC">North Carolina</SelectItem>
+                        <SelectItem value="ND">North Dakota</SelectItem>
+                        <SelectItem value="OH">Ohio</SelectItem>
+                        <SelectItem value="OK">Oklahoma</SelectItem>
+                        <SelectItem value="OR">Oregon</SelectItem>
+                        <SelectItem value="PA">Pennsylvania</SelectItem>
+                        <SelectItem value="RI">Rhode Island</SelectItem>
+                        <SelectItem value="SC">South Carolina</SelectItem>
+                        <SelectItem value="SD">South Dakota</SelectItem>
+                        <SelectItem value="TN">Tennessee</SelectItem>
+                        <SelectItem value="TX">Texas</SelectItem>
+                        <SelectItem value="UT">Utah</SelectItem>
+                        <SelectItem value="VT">Vermont</SelectItem>
+                        <SelectItem value="VA">Virginia</SelectItem>
+                        <SelectItem value="WA">Washington</SelectItem>
+                        <SelectItem value="WV">West Virginia</SelectItem>
+                        <SelectItem value="WI">Wisconsin</SelectItem>
+                        <SelectItem value="WY">Wyoming</SelectItem>
+                      </SelectContent>
+                    </Select>
                   </div>
                 </div>
+                <div className="self-stretch justify-start items-start gap-2 inline-flex">
+                  <Input
+                    variant="default"
+                    label="Zip/Postal code"
+                    id="zip"
+                    name="zip"
+                    type="text"
+                    className="flex-1" />
+                  <div className="flex-1">
+                    <Select>
+                      <SelectTrigger label="Country">
+                        <SelectValue placeholder="Select one" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="USA">United States of America</SelectItem>
+                        <SelectItem value="CAN">Canada</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </div>
+
                 <Button variant='signUp' onClick={handleNext}>Next</Button>
               </div>
               <div className='flex flex-col justify-center w-full items-center absolute bottom-0 px-6 pb-6 pt-4'>
@@ -357,6 +381,21 @@ export default function FreePlanForm() {
         </div>
       )}
 
+      {step === 6 && (
+        <div className='flex w-full h-screen justify-center items-center'>
+          <div className='flex flex-col gap-8'>
+            <div className="w-96 h-[84px] flex-col justify-start items-start gap-3 inline-flex">
+              <div className="self-stretch h-[84px] flex-col justify-start items-start gap-2 flex">
+                <div className="text-center text-gray-900 text-3xl font-semibold leading-9">You’re ready to go!</div>
+                <div className="self-stretch justify-start items-center gap-1 inline-flex">
+                  <div className="grow shrink basis-0 text-gray-600 text-sm font-normal leading-tight">Your account was successfully created. Click below to start managing your parking community.</div>
+                </div>
+              </div>
+            </div>
+            <Button variant='signUp' onClick={handlePrevious}>Sign In</Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
