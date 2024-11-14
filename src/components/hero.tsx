5cba9795cd404e144a1246f5e7d21a94249c3486
zachcygan <zachcygan@gmail.com>
2024-11-14 11:09:44 -0700

    removed font

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,14 +1,7 @@
 'use client'
 import { useState } from 'react'
 import { Button } from './button'
-import localFont from 'next/font/local'
 import MailingListModal from './mailingListModal'
-
-const barlow = localFont({
-  src: "../app/fonts/Barlow-Medium.woff",
-  variable: "--font-barlow-medium",
-  weight: "100 900",
-})
 
 
 export default function Hero() {
@@ -18,7 +11,7 @@ export default function Hero() {
     <div>
       <div className={`bg-darkGreen pl-5 lg:pl-10 pt-10 lg:pt-28`}>
         <div className="pb-24 pt-32 sm:pb-32 sm:pt-24 md:pb-48 md:pt-36 text-tGreen">
-          <h1 className={`font-display text-balance font-medium tracking-tight text-tGreen text-6xl xs: sm:text-[7.5rem] max-w-[10ch] sm:max-w-none ${barlow.className}`}>
+          <h1 className={`font-display text-balance font-medium tracking-tight text-tGreen text-6xl xs: sm:text-[7.5rem] max-w-[10ch] sm:max-w-none`}>
             Sma<span className='tracking-[.3rem]'>rt</span> and <br /> social parking
           </h1>
           <h2 className="mt-8 max-w-2xl text-lg/7 font-medium sm:text-3xl/8">
