30b4e45020a844f58640d53cbcc487ef544d1c8d
zachcygan <zachcygan@gmail.com>
2024-11-19 21:06:46 -0700

    fixed checkmark

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,11 +1,9 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
-import { Check } from "lucide-react"
-import CheckMark from "@/components/check"
-
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import CheckMark from "@/components/check";
+import { cn } from "@/lib/utils";
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
@@ -14,20 +12,20 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "w-4 h-4 relative bg-white text-white rounded border border-gray-300",
+      "w-4 h-4 relative bg-white text-white rounded border border-gray-300 flex items-center justify-center",
       className
     )}
     {...props}
   >
     <CheckboxPrimitive.Indicator
-      className={cn("flex items-center bg-pl8Green justify-center text-current")}
+      className={cn("flex items-center justify-center")}
     >
-      <div className="w-4 h-4 pl-[3.51px] pr-[3.50px] py-[4.50px] bg-[#00695c] rounded justify-center items-center inline-flex">
+      <div className="w-4 h-4 bg-[#00695c] rounded flex items-center justify-center">
         <CheckMark />
       </div>
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
-))
-Checkbox.displayName = CheckboxPrimitive.Root.displayName
+));
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
 
-export { Checkbox }
+export { Checkbox };
