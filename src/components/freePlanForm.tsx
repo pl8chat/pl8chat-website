2e21eee61346b54b4abc1a9b87e4a1c1f3856bbb
zachcygan <zachcygan@gmail.com>
2024-11-22 20:35:21 -0700

    free sign up flow done?

--- src/components/freePlanForm.tsx
+++ src/components/freePlanForm.tsx
@@ -11,6 +11,7 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 import BackArrow from './svgs/backArrow'
+import GreenCheck from './svgs/checkGreen'
 import ProgressBar from './progressBar';
 import Link from 'next/link'
 import Image from 'next/image'
@@ -31,7 +32,7 @@ const emails = [
 ]
 
 export default function FreePlanForm() {
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(6);
   const [phoneNumber, setPhoneNumber] = useState('');
   const [values, setValues] = useState<Record<SelectKeys, string | undefined>>({
     role: undefined,
@@ -426,7 +427,29 @@ export default function FreePlanForm() {
 
       {step === 6 && (
         <div className='flex w-full h-screen justify-center items-center'>
-          <div className='flex flex-col gap-8'>
+          <div className="w-[464px] h-[220px] pl-10 rounded-xl flex-col justify-start items-start gap-0.5 inline-flex">
+            <div className="h-[220px] w-[384px] flex-col justify-start items-start gap-6 flex">
+              <div className="w-12 h-12 p-3 rounded-3xl border-2 border-[#034b48] justify-center items-center inline-flex">
+                <GreenCheck />
+              </div>
+              <div className="h-[84px] flex-col justify-start items-start gap-2 flex">
+                <div className="text-center text-gray-900 texFt-3xl font-semibold leading-9">You’re ready to go!</div>
+                <div className="self-stretch justify-start items-center gap-1 inline-flex">
+                  <div className='max-w-[37ch] h-10'><span className="text-gray-900 text-xs font-normal leading-tight ">Your account was successfully created and a confirmation email has been sent to</span><span className="text-gray-900 text-sm font-normal leading-tight"> </span><span className="text-gray-900 text-sm font-bold leading-tight">michael@pl8chat.com. </span></div>
+                </div>
+              </div>
+              <div className="self-stretch justify-start items-center gap-1 inline-flex">
+                <div className="grow shrink basis-0 flex-col justify-start items-start gap-6 inline-flex">
+                  <div className="self-stretch max-w-[40ch] text-gray-900 text-sm font-normal leading-tight">Click below to start managing your parking community!</div>
+                </div>
+              </div>
+              <div className='w-[384px]'>
+                <Button variant='signUp' onClick={handlePrevious} className={`w-full`}>Sign In</Button>
+              </div>
+            </div>
+
+          </div>
+          {/* <div className='flex flex-col gap-8'>
             <div className="w-96 h-[84px] flex-col justify-start items-start gap-3 inline-flex">
               <div className="self-stretch h-[84px] flex-col justify-start items-start gap-2 flex">
                 <div className="text-center text-gray-900 text-3xl font-semibold leading-9">You’re ready to go!</div>
@@ -436,7 +459,7 @@ export default function FreePlanForm() {
               </div>
             </div>
             <Button variant='signUp' onClick={handlePrevious}>Sign In</Button>
-          </div>
+          </div> */}
         </div>
       )}
     </div>
