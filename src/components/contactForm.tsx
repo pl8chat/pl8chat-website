8ed7e0f06de69611f6dcedee23b787f411fd01c5
zachcygan <zachcygan@gmail.com>
2024-11-26 19:46:07 -0700

    fixed contact form modal width?

--- src/components/contactForm.tsx
+++ src/components/contactForm.tsx
@@ -20,8 +20,8 @@ export default function ContactForm({ isModal }: ContactFormProps) {
   const [emailTouched, setEmailTouched] = useState<boolean>(false);
 
   return (
-    <div className={`${isModal ? 'pb-0 px-10' : 'pl-24 pr-10 pb-12'} h-[882px] pt-16 bg-white flex-col justify-start items-center gap-8 flex-1 inline-flex`}>
-      <div className="h-[630px] flex-col justify-start items-center gap-8 flex max-w-1/2">
+    <div className={`${isModal ? 'p-10' : 'pl-24 pr-10 pb-12'} pt-16 bg-white flex-col justify-start items-center gap-8 flex-1 inline-flex rounded-md`}>
+      <div className="flex-col justify-start items-center gap-8 flex max-w-1/2">
         <div className="self-stretch h-[84px] flex-col justify-start items-start gap-3 flex">
           <div className="self-stretch h-[84px] flex-col justify-start items-start gap-2 flex">
             <div className="text-center text-gray-900 text-3xl font-semibold leading-9">Talk to sales</div>
@@ -30,12 +30,12 @@ export default function ContactForm({ isModal }: ContactFormProps) {
             </div>
           </div>
         </div>
-        <div className="self-stretch h-[514px] flex-col justify-start items-start gap-2.5 flex">
-          <form ref={form} className="self-stretch h-[514px] flex-col justify-start items-start gap-6 flex">
+        <div className="self-stretch flex-col justify-start items-start gap-2.5 flex">
+          <form ref={form} className="self-stretch flex-col justify-start items-start gap-6 flex">
             <div className="self-stretch h-[386px] flex-col justify-start items-start gap-6 flex">
               <div className="self-stretch justify-start items-center gap-6 inline-flex">
-                <Input variant='contact' label='First name' name='firstName'/>
-                <Input variant='contact' label='Last name' name='lastName'/>
+                <Input variant='contact' label='First name' name='firstName' />
+                <Input variant='contact' label='Last name' name='lastName' />
               </div>
               <Input
                 variant='contact'
@@ -69,15 +69,16 @@ export default function ContactForm({ isModal }: ContactFormProps) {
                   </Select>
                 </div>
               </div>
-              <Textarea variant='message' label='Message' className='h-20' name='message'/>
+              <Textarea variant='message' label='Message' className='h-20' name='message' />
             </div>
             <div className="self-stretch justify-start items-center gap-1 inline-flex">
               <div className="grow shrink basis-0"><span className="text-gray-600 text-sm font-normal leading-tight">By submitting my personal data, I consent to PL8CHAT collecting, processing and storing my information in accordance with the</span><span className="text-[#00695c] text-sm font-normal leading-tight"> </span><span className="text-[#00695c] text-sm font-normal underline leading-tight">PL8CHAT Privacy Policy.</span><span className="text-[#00695c] text-sm font-normal leading-tight"> </span></div>
             </div>
-            <div className="h-10 pb-3 bg-[#00695c] rounded-md shadow justify-center items-center inline-flex w-full">
-              <Button variant='signUp' className='w-full' >Talk to sales</Button>
-            </div>
+
           </form>
+        </div>
+        <div className="bg-[#00695c] rounded-md flex justify-center items-center w-full">
+          <Button variant='signUp' className='w-full' >Talk to sales</Button>
         </div>
       </div>
     </div>
