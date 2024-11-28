01f35175f19bf918b1e8b2f8683899b57284a96b
zachcygan <zachcygan@gmail.com>
2024-11-27 17:13:51 -0700

    logo alignment

--- src/app/(noNav)/premium/page.tsx
+++ src/app/(noNav)/premium/page.tsx
@@ -6,7 +6,7 @@ import GreenLogo from '@/components/svgs/logoGreen'
 export default function FreePlan() {
   return (
     <div className="fixed h-screen w-full bg-[#F6F6F4] flex flex-col justify-center items-center overflow-hidden">
-      <div className="absolute top-0 left-0 z-50 px-8 py-4">
+      <div className="absolute top-0 left-0 z-50 px-8 py-[15px] h-[58px]">
         <Link href="/">
           <GreenLogo />
         </Link>
