3b2caa2bd35e90e84cfe4bbc42c8a32884f4620d
zachcygan <zachcygan@gmail.com>
2024-11-27 17:04:52 -0700

    save

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
   return (
     <div>
       <div className="h-[683px] w-full px-8 pt-32 pb-[120px] bg-[#034b48] flex-col justify-start items-start gap-10 inline-flex">
-        <div className="w-[738px] text-white text-[100px] font-medium leading-[100px]">Smart and Social Parking </div>
+        <div className="max-w-[12ch] text-white text-[100px] font-medium leading-[100px]">Smart and Social Parking </div>
         <div className="w-[768px] text-white text-3xl font-normal leading-[35px]">We’re a new line of communication connecting<br />people, cars and parking communities through <br />license plates.  </div>
         <Button variant="secondary" onClick={() => setModalOpen(true)}>Learn more</Button>
       </div>
