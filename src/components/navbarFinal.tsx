0e3fc807efc1b674fcb59e5e7661ede09f6b3d19
Zach <vercel.clinic576@slmails.com>
2024-12-02 20:20:10 -0700

    typo

--- src/components/navbarFinal.tsx
+++ src/components/navbarFinal.tsx
@@ -228,7 +228,7 @@ export default function NavbarFinal() {
 
               {/* Right-side items (shown on larger screens) */}
               <div className="sm:flex sm:items-center text-sm font-normal">
-                <div className="hidden lg:flex lg:items-center gap-4">
+                <div className="hidden lg:flex lg:items-center gap-4 ">
                   <div className={getTextColor().replace(/hover:\S+/g, '')}>
                     Call us: 1(310)PL8-CHAT
                   </div>
