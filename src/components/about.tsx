ad30b8f1be3fc0dbc83960d98086eb6a6ee8a2b9
zachcygan <zachcygan@gmail.com>
2024-12-05 17:57:19 -0700

    fix line ehight

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -7,7 +7,7 @@ export default function AboutComponent() {
       <div className="grow shrink basis-0 justify-start items-start gap-[49px] flex w-full mt-20">
         <div className="flex-col basis-6/12 justify-start items-start gap-8 inline-flex">
           <div className="self-stretch text-[#034b48] text-[65px] font-semibold leading-[72px]">We’ll never stop asking, “What else can a license <br />plate do?”</div>
-          <div className="self-stretch text-gray-500 text-2xl font-normal leading-loose">We believe the license plate is an underutilized code<br />that’s been neglected for too long and we’re here to <br />do something about it. <br /></div>
+          <div className="self-stretch text-gray-500 text-2xl font-normal leading-8">We believe the license plate is an underutilized code<br />that’s been neglected for too long and we’re here to <br />do something about it. <br /></div>
         </div>
         <div className="basis-5/12 size-full h-[743px] relative text-black -top-24">
           <Image 
@@ -20,7 +20,7 @@ export default function AboutComponent() {
       </div>
       <div className="w-[856px] h-[457px] flex-col justify-start items-start gap-8 flex -mt-64">
         <div className="self-stretch text-[#034b48] text-[65px] font-semibold leading-[72px]">Our Mission</div>
-        <div className="self-stretch text-gray-500 text-2xl font-normal leading-loose">We believe the license plate is an underutilized code<br />that’s been neglected for too long and we’re here to <br />do something about it. <br /></div>
+        <div className="self-stretch text-gray-500 text-2xl font-normal leading-8">We believe the license plate is an underutilized code<br />that’s been neglected for too long and we’re here to <br />do something about it. <br /></div>
       </div>
     </div>
   )
