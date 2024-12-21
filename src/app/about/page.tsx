d0d19ea00f30859c407a10b016bdae3a6b57888b
Zach <vercel.clinic576@slmails.com>
2024-12-20 21:31:03 -0700

    font size change

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ export default function About() {
     <div className='bg-[#f3f4f2]'>
       <div className="pl-[140px] pt-[184px] justify-start items-start inline-flex flex-col gap-4 pb-[16px] w-full">
         <div className="text-[#034b48] text-[45px] font-semibold leading-[100px]">About us</div>
-        <div className='h-[440px]'><span className="text-black text-xl font-normal leading-10">PL8CHAT is a technology company transforming license <br />plates into the universal line of vehicle communication.<br /><br />We believe through license plate innovation and simple <br />human interactions we can create a safe, accountable <br />and convenient parking and driving life for all.<br /><br />Join us on our mission to get </span><Link href={`#`} className="text-[#034b48] text-base font-semibold leading-10">Every Car Connected™</Link><span className="text-gray-500 text-2xl font-normal leading-10">.<br /><br /></span></div>
+        <div className='h-[440px]'><span className="text-black text-xl font-normal leading-10">PL8CHAT is a technology company transforming license <br />plates into the universal line of vehicle communication.<br /><br />We believe through license plate innovation and simple <br />human interactions we can create a safe, accountable <br />and convenient parking and driving life for all.<br /><br />Join us on our mission to get </span><Link href={`#`} className="text-[#034b48] text-xl font-semibold leading-10">Every Car Connected™</Link><span className="text-gray-500 text-2xl font-normal leading-10">.<br /><br /></span></div>
       </div>
     </div>
   )
