cfcafab88821223b1c00edf7146cb910b9fa75f8
zachcygan <zachcygan@gmail.com>
2024-12-21 19:06:25 -0700

    about and contact mobile

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,10 +5,17 @@ import Link from 'next/link'
 
 export default function About() {
   return (
-    <div className='bg-[#f3f4f2]'>
-      <div className="pl-[140px] pt-[184px] justify-start items-start inline-flex flex-col gap-4 pb-[16px] w-full">
-        <div className="text-[#034b48] text-[45px] font-semibold leading-[100px]">About us</div>
-        <div className='h-[440px]'><span className="text-black text-xl font-normal leading-10">PL8CHAT is a technology company transforming license <br />plates into the universal line of vehicle communication.<br /><br />We believe through license plate innovation and simple <br />human interactions we can create a safe, accountable <br />and convenient parking and driving life for all.<br /><br />Join us on our mission to get </span><Link href={`#`} className="text-[#034b48] text-xl font-semibold leading-10">Every Car Connected™</Link><span className="text-gray-500 text-2xl font-normal leading-10">.<br /><br /></span></div>
+    <div className='bg-[#d1d5dc] md:bg-[#f3f4f2]'>
+      <div className="px-4 md:pl-[140px] pt-6 md:pt-[184px] justify-start items-start inline-flex flex-col gap-4 pb-20 md:pb-[16px] w-full">
+        <div className="text-[#034b48] text-xl md:text-[45px] font-semibold leading-[50px] md:leading-[100px]">About us</div>
+        {/* Mobile */}
+        <div className='md:hidden'>
+          <div className='md:h-[440px]'><span className="text-black text-sm font-normal leading-[25px]">PL8CHAT is a technology company transforming license plates into the universal line of vehicle communication.<br /><br />We believe through license plate innovation and simple human interactions we can create a safe, accountable and convenient parking and driving life for all. <br /><br /> Join us on our mission to get </span><Link href={`#`} className="text-[#034b48] text-sm font-semibold leading-[25px]">Every Car Connected™</Link></div>
+        </div>
+        {/* Desktop */}
+        <div className='hidden md:block'>
+          <div className='md:h-[440px]'><span className="text-black text-xl font-normal leading-10">PL8CHAT is a technology company transforming license <br />plates into the universal line of vehicle communication.<br /><br />We believe through license plate innovation and simple <br />human interactions we can create a safe, accountable <br />and convenient parking and driving life for all.<br /><br />Join us on our mission to get </span><Link href={`#`} className="text-[#034b48] text-xl font-semibold leading-10">Every Car Connected™</Link></div>
+        </div>
       </div>
     </div>
   )
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,10 +3,17 @@ import Link from 'next/link'
 
 export default function Contact() {
   return (
-    <div className='bg-[#f3f4f2]'>
-      <div className="pl-[140px] pt-[184px] justify-start items-start inline-flex flex-col gap-4 pb-[16px] w-full">
-        <div className="text-[#034b48] text-[45px] font-semibold leading-[100px]">Let's connect</div>
-        <div className="w-[1058px] h-[440px]"><span className="text-black text-xl font-normal leading-10">Questions? Comments? Something else? We’ love to hear from you.<br /></span><Link href={'mailto:info@pl8chat.com'} className="text-[#034b48] text-base font-semibold leading-10">info@pl8chat.com</Link><span className="text-black text-base font-normal leading-10">.<br /></span><span className="text-[#034b48] text-base font-semibold leading-10"><br /></span><span className="text-black text-base font-normal leading-10"><br /><br /><br /><br /><br /><br /><br /></span></div>
+    <div className='bg-[#d1d5dc] md:bg-[#f3f4f2]'>
+      <div className="px-4 h-[445px] md:pl-[140px] pt-6 md:pt-[184px] justify-start items-start inline-flex flex-col gap-4 pb-[16px] w-full">
+        <div className="text-[#034b48] text-xl md:text-[45px] font-semibold leading-[50px] md:leading-[100px]">Let's connect</div>
+        {/* Mobile */}
+        <div className='md:hidden'>
+          <div className="md:w-[1058px] md:h-[440px]"><span className="text-black text-sm font-normal leading-[25px]">Questions? Comments? Something else? We’ love to hear from you.<br /></span><Link href={'mailto:info@pl8chat.com'} className="text-[#034b48] text-sm font-semibold leading-[25px]">info@pl8chat.com</Link><span className="text-black text-base font-normal leading-[25px]">.<br /></span><span className="text-[#034b48] text-base font-semibold leading-[25px]"><br /></span><span className="text-black text-sm font-normal leading-[25px]"></span></div>
+        </div>
+        {/* Desktop */}
+        <div className='hidden md:block'>
+          <div className="md:w-[1058px] h-[440px]"><span className="text-black text-xl font-normal leading-10">Questions? Comments? Something else? We’ love to hear from you.<br /></span><Link href={'mailto:info@pl8chat.com'} className="text-[#034b48] text-base font-semibold leading-10">info@pl8chat.com</Link><span className="text-black text-base font-normal leading-10">.<br /></span><span className="text-[#034b48] text-base font-semibold leading-10"><br /></span><span className="text-black text-base font-normal leading-10"><br /><br /><br /><br /><br /><br /><br /></span></div>
+        </div>
       </div>
     </div>
   )
