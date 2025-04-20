b63101d10c88119d21fd000775d6fb4f60b2e304
zachcygan <zachcygan@gmail.com>
2025-04-20 11:22:08 -0600

    save

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,6 +2,7 @@
 import { useState } from 'react'
 import { Button } from './ui/button'
 import localFont from "next/font/local";
+import Image from "next/image";
 import MailingListModal from './mailingListModal'
 
 const inter = localFont({
@@ -14,26 +15,20 @@ export default function Hero() {
   const [isModalOpen, setModalOpen] = useState<boolean>(false);
 
   return (
-    <div className='w-full'>
-      <div className="h-[560px] md:h-[683px] w-full px-4 md:px-8 pt-[56px] lg:pt-32 md:pb-[120px] bg-[#f3f4f2] flex-col justify-start items-start gap-4 md:gap-10 inline-flex">
-        {/* <div className="max-w-[12ch] text-white text-[100px] font-medium leading-[100px]">Smart and Social Parking </div> */}
-        <div className="md:w-[822px] w-[320px] pt-16">
-          <span
-            className="text-[#034b48] text-[60px] md:text-[100px] font-medium md:font-normal leading-[50px] md:leading-[100px]"
-          >
-            Smart and Social Parking
-          </span>
-          <span
-            className={`text-[#034b48] text-[35px] md:text-[100px] font-medium md:font-normal leading-[50px] md:leading-[100px] ${inter.className}`}
-            style={{ display: 'inline-block', verticalAlign: 'baseline' }}
-          >
-            ™
-          </span>
+    <div className='w-full pb-[120px] flex'>
+      <div className='flex-1'>
+        <div className="h-[560px] md:h-[683px] w-full px-4 md:px-8 pt-[56px] lg:pt-32 md:pb-[120px]  flex-col justify-start items-start gap-4 md:gap-10 inline-flex">
+          <div className="md:w-[822px] w-[320px] pt-16">
+            <div className="w-[822px] justify-start text-[#363636] text-8xl font-medium leading-[90px]">The Voice <br />of Cars</div>
+          </div>
+          <div className="w-[798px] justify-start text-neutral-700 text-3xl font-normal leading-9">We’re a vehicle connectivity platform <br />transforming license plates into a new <br />line of communication.</div>
+          <Button variant="secondary" className={``} onClick={() => setModalOpen(true)}>Learn more</Button>
         </div>
-        <div className="w-[316px] md:w-[768px] text-[#353535] text-[17px] md:text-[29px] font-normal leading-[25px] md:leading-[35px] pb-2">We’re a new line of communication connecting people, cars and parking communities through license plates.  </div>
-        <Button variant="secondary" className={``} onClick={() => setModalOpen(true)}>Learn more</Button>
+        <MailingListModal isOpen={isModalOpen} onClose={() => setModalOpen(false)} />
       </div>
-      <MailingListModal isOpen={isModalOpen} onClose={() => setModalOpen(false)} />
+      <div className='flex-1 bg-[#f3f4f2] relative'>
+        <Image src={'/assets/images/License Plate.png'} fill alt='License Plate' />
+      </div>
     </div>
   )
 }
