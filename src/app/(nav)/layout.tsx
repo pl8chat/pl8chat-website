358c7c2c2255655b1f98bc9c857ca50c73cc142a
zachcygan <zachcygan@gmail.com>
2025-06-22 18:41:00 -0600

    navbar margin

--- src/app/(nav)/layout.tsx
+++ src/app/(nav)/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <header>
           <NavbarFinal />
         </header>
-        <div className="pt-[58px] bg-white">
+        <div className="pt-[70px] bg-white">
           {children}
           <Analytics />
         </div>
