83bb7c5dbae6a9d98c972f13aa35a9cf79be5b58
zachcygan <zachcygan@gmail.com>
2025-07-09 18:27:51 -0600

    please?

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
+// ✅ ESM-compatible next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
+      test: /\.svg$/,
+      issuer: { and: [/\.(js|ts)x?$/] },
       use: ['@svgr/webpack'],
     });
-
     return config;
   },
 };
