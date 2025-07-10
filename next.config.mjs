9c02a03be61be86b1065329adb32c82fd7acb211
zachcygan <zachcygan@gmail.com>
2025-07-09 18:26:12 -0600

    :?

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/, // only apply to JS/TS files
       use: ['@svgr/webpack'],
     });
-
     return config;
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
