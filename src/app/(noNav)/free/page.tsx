73fa5ce01de2c094daa59732606863340ac6e7a4
zachcygan <zachcygan@gmail.com>
2025-07-09 18:40:57 -0600

    should be fixed now

--- src/app/(noNav)/free/page.tsx
+++ src/app/(noNav)/free/page.tsx
@@ -8,7 +8,7 @@ export default function FreePlan() {
     <div className="fixed h-screen w-full bg-[#F6F6F4] flex flex-col justify-center items-center overflow-hidden">
       <div className="absolute top-0 left-0 z-50 px-8 py-[15px]">
         <Link href="/">
-          <Pl8chatGreen />
+          <Image src="/assets/svgs/pl8chatGreen.svg" alt="Logo" width={124} height={28} />
         </Link>
       </div>
       <FreePlanForm />
--- src/app/(noNav)/resetpassword/page.tsx
+++ src/app/(noNav)/resetpassword/page.tsx
@@ -1,5 +1,6 @@
 import ResetPasswordForm from "@/components/resetPasswordForm"
 import Link from 'next/link'
+import Image from 'next/image'
 import Pl8chatGreen from '@/svgs/pl8chatGreen.svg'
 
 export default function Resetpassword() {
@@ -7,7 +8,7 @@ export default function Resetpassword() {
     <div className="fixed h-screen w-full bg-[#F6F6F4] flex flex-col justify-center items-center overflow-hidden">
       <div className="absolute top-0 left-0 z-50 px-8 py-[15px] h-[58px]">
         <Link href="/">
-          <Pl8chatGreen />
+          <Image src="/assets/svgs/pl8chatGreen.svg" alt="Logo" width={124} height={28} />
         </Link>
       </div>
       <ResetPasswordForm />
