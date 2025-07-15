7caa812b46f582f8c73eed519846c5015273bd71
Zach Cygan <coding@cyganmail.com>
2025-07-15 17:01:29 -0600

    coming soon

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -85,55 +85,8 @@ export default function Hero() {
 
   return (
     <div className=''>
-      {/* Side by side w/ image */}
-      {/* <div className='w-full md:flex h-[741px] bg-[#f3f4f2] hidden'>
-        <div className='basis-1/2'>
-          <div className="h-[560px] md:h-[751px] w-full px-4 md:pl-8 pt-[56px] lg:pt-32 md:pb-[120px] flex-col justify-start items-start gap-4 md:gap-10 inline-flex">
-            <div className="w-full max-w-[822px] pt-16">
-              <div className="w-full justify-start text-[#363636] text-8xl font-medium leading-[90px]">
-                The Voice <br />of Cars
-              </div>
-            </div>
-            <div className="w-full max-w-[798px] justify-start text-neutral-700 text-3xl font-normal leading-9">
-              We’re a vehicle connectivity platform <br />transforming license plates into a new <br />line of communication.
-            </div>
-            <Button variant="secondary" onClick={() => setModalOpen(true)}>Learn more</Button>
-          </div>
-          <MailingListModal isOpen={isModalOpen} onClose={() => setModalOpen(false)} />
-        </div>
-        <div className='basis-1/2 bg-[#f3f4f2] relative'>
-          <Image
-            src={'/assets/images/License Plate.png'}
-            alt='License Plate'
-            fill
-            objectFit='cover'
-            className="absolute top-0 left-0 w-full h-full"
-          />
-           <div className="absolute top-0 left-0 w-full h-[25%] bg-gradient-to-b from-black/60 to-transparent" />
-        </div>
-      </div>
-      <div className="md:hidden flex bg-[#f3f4f2] w-full relative h-[654px]">
-        <div className="absolute inset-0 bg-[url('/assets/images/License%20Plate%202.png')] bg-cover bg-center" />
-        <div className="absolute inset-0 bg-black/60" />
-
-        <div className='pt-[91px]'>  
-          <div className="relative w-full px-4 pt-[56px] pb-[120px] flex-col justify-start items-start gap-6 inline-flex">
-            <div className="w-80 text-white text-5xl font-medium leading-[50px]">
-              The Voice <br />of Cars
-            </div>
-            <div className="w-80 text-white text-xl font-medium leading-7">
-              We’re a vehicle connectivity <br />
-              <span className="leading-loose">
-                platform transforming license plates into a new line of communication.
-              </span>
-            </div>
-            <Button variant="secondary" onClick={() => setModalOpen(true)}>Learn more</Button>
-            <MailingListModal isOpen={isModalOpen} onClose={() => setModalOpen(false)} />
-          </div>
-        </div>
-      </div> */}
-      <div className='mx-auto w-full flex h-[100vh] justify-center items-start md:items-center bg-[#E8F4F0] z-30 pt-[109px] md:pt-0'>
-        <div className='flex-col justify-center'>
+      <div className='flex flex-col items-center justify-center h-[100vh] w-full bg-[#E8F4F0] z-30 px-4'>
+        <div className='flex-col justify-center items-center'>
           <div className='flex justify-center'>
             <div className='hidden md:flex'>
               <HeroText />
@@ -142,7 +95,7 @@ export default function Hero() {
               <HeroTextMobile />
             </div>
           </div>
-          <div className="pl-[5px] inline-flex flex-col justify-center items-center gap-[50px] pt-[50px] md:pt-[72px]">
+          {/* <div className="pl-[5px] inline-flex flex-col justify-center items-center gap-[50px] pt-[50px] md:pt-[72px]">
             <div className="w-[350px] md:w-[905px] text-center justify-center text-[#002823] text-[37px] md:text-[50px] font-medium leading-[34px] md:leading-[60px]">We’re a new kind of auto club. Built on<br />communication and community. </div>
             <form ref={form} onSubmit={handleFormSubmit} noValidate className='md:-translate-x-5'>
               <div className="w-full flex flex-col items-center justify-center md:justify-start md:items-start gap-2 md:flex-row md:w-[478px]">
@@ -164,7 +117,6 @@ export default function Hero() {
                           setEmail(e.target.value);
                         }}
                       />
-                      {/* Mobile */}
                       <div className='flex flex-col md:hidden leading-[18px] text-[11px]'>
                         {emailTouched && validEmail && (
                           <div className='pt-[10px] text-red-500'>
@@ -181,7 +133,6 @@ export default function Hero() {
                       <div className="justify-start text-white text-base font-medium leading-normal tracking-tight">Join waitlist</div>
                     </button>
                   </div>
-                  {/* Desktop */}
                   <div className='relative w-full'>
                     <div className={`hidden text-xs ${emailTouched && validEmail ? 'md:flex flex-col' : ''} ${!validEmail ? 'md:flex flex-col' : ''} absolute`}>
                       {emailTouched && validEmail && (
@@ -201,11 +152,11 @@ export default function Hero() {
                     </div>
                   </div>
                   </div>
-                {/* <button data-state="Default" data-type="Primary" className="w-36 h-12 px-5 py-3 bg-emerald-950 rounded-2xl outline outline-1 outline-offset-[-1px] outline-emerald-950 flex justify-center items-center overflow-hidden">
-                  <div className="justify-start text-white text-base font-medium leading-normal tracking-tight">Join waitlist</div>
-                </button> */}
               </div>
             </form>
+          </div> */}
+          <div className='text-2xl md:text-[36px] font-regular text-black text-center pt-5'>
+            coming soon
           </div>
         </div>
       </div>
