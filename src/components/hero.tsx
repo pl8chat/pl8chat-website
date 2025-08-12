51f1be3bde2e1924d866e8dce47f7252a69bbf63
zachcygan <zachcygan@gmail.com>
2025-08-12 15:34:51 -0600

    updated hero img position

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,7 +4,9 @@ import Link from 'next/link';
 export default function Hero({ }) {
   return (
     <div className='flex justify-center px-4 md:px-0  pt-[32px] md:pt-[50px] pb-4 md:pb-5'>
-      <div className="w-full md:w-[1400px] h-[500px] md:h-[700px] pl-[18px] md:pl-[90px] md:pr-28 pt-[130px] md:pt-[180px] md:pb-[145px] rounded-3xl inline-flex flex-col justify-start items-start gap-7 bg-[linear-gradient(to_left,_rgba(0,0,0,0)_10%,_rgba(0,0,0,0.6)_90%,_rgba(0,0,0,0.6)_100%),url('/assets/images/HeroBg.png')] md:bg-[linear-gradient(to_left,_rgba(0,0,0,0)_0%,_rgba(0,0,0,0.6)_90%,_rgba(0,0,0,0.6)_100%),url('/assets/images/HeroBg.png')] bg-cover bg-center bg-no-repeat">
+      <div
+        className="w-full md:w-[1400px] h-[500px] md:h-[700px] pl-[18px] md:pl-[90px] md:pr-28 pt-[130px] md:pt-[180px] md:pb-[145px] rounded-3xl inline-flex flex-col justify-start items-start gap-7 bg-[linear-gradient(to_left,_rgba(0,0,0,0)_10%,_rgba(0,0,0,0.6)_90%,_rgba(0,0,0,0.6)_100%),url('/assets/images/HeroBg.png')] md:bg-[linear-gradient(to_left,_rgba(0,0,0,0)_0%,_rgba(0,0,0,0.6)_90%,_rgba(0,0,0,0.6)_100%),url('/assets/images/HeroBg.png')] bg-cover bg-no-repeat bg-[position:0%_0%,_center_-12.5rem]"
+      >
         <div className="w-[288px] md:w-[768px] flex flex-col justify-start items-start gap-10">
           <div className="self-stretch flex flex-col justify-start items-start gap-4">
             <div className="justify-start text-white text-[36px] md:text-6xl font-medium leading-[40px] md:leading-[58px]">Keep your car<br />safe and <br />connected.</div>
