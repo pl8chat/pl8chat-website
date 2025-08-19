860b2c896dc8648ee969e24a3e910df1757b7683
zachcygan <zachcygan@gmail.com>
2025-08-19 16:39:35 -0600

    404 page mobile friendly

--- src/app/(nav)/not-found.tsx
+++ src/app/(nav)/not-found.tsx
@@ -1,10 +1,10 @@
 export default function NotFound() {
   return (
-    <div className="pl-36 pt-44 inline-flex flex-col gap-4">
-      <div className="text-black text-4xl font-semibold leading-[60px]">Oops!</div>
-      <div className="w-[897px] md:h-[350px] 2xl:h-[681px]">
-        <span className="text-black text-lg">We can’t find the page you’re looking for.<br /></span>
-        <span className="text-black text-lg font-semibold">Error code: 404<br /></span>
+    <div className="pl-4 md:pl-36 pt-6 md:pt-44 inline-flex flex-col gap-4">
+      <div className="text-black text-[30px] md:text-4xl font-semibold leading-[60px]">Oops!</div>
+      <div className="md:w-[897px] h-[400px] md:h-[350px] 2xl:h-[681px]">
+        <span className="text-black text-sm md:text-lg">We can’t find the page you’re looking for.<br /></span>
+        <span className="text-black text-sm md:text-lg font-semibold">Error code: 404<br /></span>
       </div>
     </div>
   );
