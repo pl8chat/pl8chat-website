25ff87449cd77f51a2394a73acb1ae4353ef52e4
zachcygan <zachcygan@gmail.com>
2025-08-19 15:09:29 -0600

    updated privacy page

--- src/app/(nav)/privacy/page.tsx
+++ src/app/(nav)/privacy/page.tsx
@@ -1,97 +1,79 @@
-import Link from 'next/link'
-
-interface PrivacySection {
-  title: string;
-  content?: string;
-  isHighlight?: boolean;
-}
-
-const privacyData: PrivacySection[] = [
-  {
-    title: '1. Overview',
-    content:
-      '1.1 Welcome to PL8CHAT (the "App"). This Privacy Policy explains how we collect, use, disclose, and safeguard your personal information when you use our App.',
-  },
-  {
-    title: '2. Information We Collect',
-    content:
-      '2.1 Personal Information:\nWhen you register for an account, we collect your name, phone number, email address and license plate number.\nWhen you create a profile, you may choose to provide additional information such as your full name and vehicle details.\nWe may collect information about your interactions with other users, content you post, and messages you send.\n\n2.2 Automatically Collected Information:\nWe automatically collect certain information about your device, including device type and operating system.\nWe may use cookies and similar technologies to enhance your experience and gather information about your usage patterns.',
-  },
-  {
-    title: '3. How We Use Your Information',
-    content:
-      "3.1 We use the information we collect for the following purposes:\nTo provide and personalize the App's features.\nTo communicate with you, including sending important notices, updates, and promotional materials.\nTo monitor and analyze usage patterns and improve the App's functionality.\nTo prevent fraud, enforce our terms, and comply with legal obligations.",
-  },
-  {
-    title: '4. Information Sharing',
-    content:
-      "4.1 We do not sell, trade, or otherwise transfer your personal information to third parties without your consent, except as described in this Privacy Policy.\n4.2 We may share your information with:\nService providers who assist us in operating the App and providing services.\nOther users as part of the App's features such as sharing vehicle and user profiles and messaging.",
-  },
-  {
-    title: '5. Your Choices',
-    content:
-      '5.1 You can control and customize certain privacy settings within the App.\n5.2 You may opt out of receiving promotional communications from us by following the instructions in those communications or by contacting us directly.',
-  },
-  {
-    title: '6. Security',
-    content:
-      '6.1 We take reasonable measures to protect your personal information from unauthorized access, use, or disclosure.',
-  },
-  {
-    title: "7. Children's Privacy",
-    content:
-      '7.1 The App is not intended for individuals under the age of 16, and we do not knowingly collect personal information from children.',
-  },
-  {
-    title: '8. Changes to This Privacy Policy',
-    content:
-      '8.1 We reserve the right to update or modify this Privacy Policy at any time. We will notify you of any changes by posting the new policy on this page.',
-  },
-];
-
-
 export default function Terms() {
   return (
     <div className="self-stretch px-4 md:pl-36 pt-6 md:pt-44 pb-7 md:pb-20 inline-flex flex-col justify-start items-start gap-4">
       <div className="self-stretch justify-start text-black text-xl md:text-3xl font-semibold leading-[50px] md:leading-[100px]">
         Privacy Policy
       </div>
-      <div className="md:w-[897px] justify-start">
-        <div className='flex flex-col pb-4'>
-          <div className='text-black text-sm font-semibold leading-[25px] mb-1'>
-            Last updated: December 01, 2024
-          </div>
-          <div className='text-black text-sm font-normal leading-[25px] whitespace-pre-line'>
-            We started PL8CHAT to connect people, cars, and unite parking and driving communities through license plates. Our mission is to bring the automotive community closer than ever before, and we are committed to valuing your privacy as we do so. This Privacy Policy describes how PL8CHAT, LLC. and its affiliates (“we,” “us,” or “PL8CHAT”), process personal and vehicle information in relation to your use of the PL8CHAT Platform.
-          </div>
-        </div>
-        {privacyData.map((section, idx) => (
-          <div key={idx} className="mb-4">
-            <div className="text-black text-sm font-semibold leading-[25px] mb-1">
-              {section.title}
-            </div>
-            {section.content && (
-              <div className="text-black text-sm font-normal leading-[25px] whitespace-pre-line">
-                {section.content}
-              </div>
-            )}
-          </div>
-        ))}
-        <div className="mt-6 text-black text-sm font-semibold leading-[25px]">
-          Contact Us
-        </div>
-        <div className="text-black text-sm font-normal leading-[25px]">
-          If you have any questions or concerns about this Privacy Policy, please contact us at:
-          <br />
-          +1 (310) PL8-CHAT
-        </div>
-        <Link
-          href="mailto:info@pl8chat.com"
-          className="text-[#034B48] text-sm font-semibold leading-[25px]"
-        >
-          info@pl8chat.com
-        </Link>
-      </div>
+      <div className="md:w-[897px] justify-start"><span className="text-black text-sm font-semibold leading-normal">Last updated: August 21, 2025<br /><br /></span><span className="text-black text-sm font-normal leading-normal">We started PL8CHAT to connect people, cars, and unite parking and driving communities through license plates. Our mission is to bring the automotive community closer than ever before, and we are committed to valuing your privacy as we do so. This Privacy Policy describes how PL8CHAT, LLC. and its affiliates (“we,” “us,” or “PL8CHAT”) process personal and vehicle information in relation to your use of the PL8CHAT Platform.<br /></span><span className="text-black text-sm font-semibold leading-normal"><br />Overview<br /></span><span className="text-black text-sm font-normal leading-normal">This Privacy Policy explains how we collect, use, disclose, and safeguard your personal information when you use our App.<br /></span>
+        <span className="text-black text-sm font-semibold leading-normal"><br />Information We Collect<br /></span>
+        <span className="text-black text-sm font-normal leading-normal">When you register for an account, we collect:<br /></span>
+        <ul className='text-black text-sm font-normal leading-normal list-disc translate-x-7'>
+          <li>Name</li>
+          <li>Phone number</li>
+          <li>Email address</li>
+        </ul>
+        <span className="text-black text-sm font-semibold leading-normal"><br />To verify your identity before adding a vehicle, we collect:<br /></span>
+        <ul className='text-black text-sm font-normal leading-normal list-disc translate-x-7'>
+          <li>A photo of your government-issued ID (such as a driver’s license or state ID)</li>
+          <li>A selfie photo to confirm your identity matches the ID</li>
+        </ul>
+        <span className="text-black text-sm font-semibold leading-normal"><br />If you choose to add a vehicle, we collect:<br /></span>
+        <ul className='text-black text-sm font-normal leading-normal list-disc translate-x-7'>
+          <li>License plate number</li>
+          <li>A photo of your vehicle registration to verify your license plate and confirm that the name on the registration matches your ID</li>
+        </ul>
+        <span className="text-black text-sm font-semibold leading-normal"><br />We also automatically collect certain technical information, including:<br /></span>
+        <ul className='text-black text-sm font-normal leading-normal list-disc translate-x-7'>
+          <li>Device type</li>
+          <li>Operating system</li>
+        </ul> <br />
+        <span className="text-black text-sm font-semibold leading-normal">Cookies and Tracking Technologies<br /></span><span className="text-black text-sm font-normal leading-normal">At this time, we do not use cookies or similar tracking technologies on our website or App. In the future, if we introduce cookies (for example, to analyze site traffic or improve your experience), we will update this Privacy Policy and, where legally required, provide you with notice and choices regarding their use.<br /></span>
+        <div className="text-black text-sm font-semibold leading-normal"><br />How We Use Your Information<br /></div>
+        <div className="text-black text-sm font-normal leading-normal">We use the information we collect to:<br /></div>
+        <ul className='text-black text-sm font-normal leading-normal list-disc translate-x-7'>
+          <li className="text-black text-sm font-normal leading-normal">Provide and personalize the App’s features</li>
+          <li className="text-black text-sm font-normal leading-normal">Verify your identity and confirm vehicle ownership</li>
+          <li className="text-black text-sm font-normal leading-normal">Communicate with you, including sending important notices, updates, and promotional materials</li>
+          <li className="text-black text-sm font-normal leading-normal">Monitor and analyze usage patterns and improve the App’s functionality</li>
+          <li className="text-black text-sm font-normal leading-normal">Prevent fraud, misuse, and unauthorized access to the Platform</li>
+          <li className="text-black text-sm font-normal leading-normal">Enforce our terms and comply with legal obligations</li>
+        </ul>
+        <span className="text-black text-sm font-semibold leading-normal"><br />Information Sharing<br /></span><span className="text-black text-sm font-normal leading-normal">We do not sell, trade, or otherwise transfer your personal information to third parties without your consent, except as described in this Privacy Policy.<br /></span>
+        <span className="text-black text-sm font-semibold leading-normal"><br />We may share your information with:<br /></span>
+        <ul className='text-black text-sm font-normal leading-normal list-disc translate-x-7'>
+          <li>Service providers who assist us in operating the App and providing services</li>
+          <li>Other users, as part of the App’s features (such as sharing vehicle and user profiles and messaging)</li>
+        </ul>
+        <span className="text-black text-sm font-semibold leading-normal"><br />Your Choices<br /></span>
+        <ul className='text-black text-sm font-normal leading-normal list-disc translate-x-7'>
+          <li>You can control and customize certain privacy settings within the App.</li>
+          <li>You may opt out of receiving promotional communications by following the instructions in those communications or by contacting us directly.</li>
+        </ul>
+        <span className="text-black text-sm font-semibold leading-normal"><br />Security<br /></span>
+        <ul className='text-black text-sm font-normal leading-normal list-disc translate-x-7'>
+          <li>We take reasonable measures to protect your personal information from unauthorized access, use, or disclosure.</li>
+          <li>Identity verification documents (such as ID photos, selfies, and vehicle registrations) are subject to additional safeguards, including encryption during transmission and storage, and access restrictions to authorized personnel only.</li>
+        </ul>
+        <span className="text-black text-sm font-semibold leading-normal"><br />Children’s Privacy<br /></span>
+        <ul className='text-black text-sm font-normal leading-normal list-disc translate-x-7'>
+          <li>The App is not intended for individuals under the age of 16.</li>
+          <li>We do not knowingly collect personal information from children under 16.</li>
+        </ul>
+        <span className="text-black text-sm font-semibold leading-normal"><br />Changes to This Privacy Policy<br /></span><span className="text-black text-sm font-normal leading-normal">We may update or modify this Privacy Policy at any time. We will notify you of any changes by posting the new policy on this page.<br /></span>
+        <span className="text-black text-sm font-semibold leading-normal"><br />Data Retention<br /></span>
+        <ul className='text-black text-sm font-normal leading-normal list-disc translate-x-7'>
+          <li>We retain your personal information for as long as necessary to provide the services, comply with legal obligations, resolve disputes, and enforce our agreements.</li>
+          <li>Identity verification documents (such as ID photos, selfies, and vehicle registrations) are retained only as long as necessary to verify your account and enforce safety measures, unless required by law to keep them longer.</li>
+          <li>You may request deletion of your information at any time by contacting us.</li>
+        </ul>
+        <span className="text-black text-sm font-semibold leading-normal"><br />Your Rights<br /></span>
+        <span className="text-black text-sm font-normal leading-normal">Depending on where you live, you may have rights under privacy laws, such as the right to:<br /></span>
+        <ul className='text-black text-sm font-normal leading-normal list-disc translate-x-7'>
+          <li>Request access to the personal information we hold about you</li>
+          <li>Request deletion of your personal information</li>
+          <li>Correct inaccurate information</li>
+        </ul>
+        <span className="text-black text-sm font-normal leading-normal">To exercise these rights, please contact us at <a href={'mailto:info@pl8chat.com'}> info@pl8chat.com.</a><br /></span><span className="text-black text-sm font-semibold leading-normal"><br />Third-Party Links<br /></span><span className="text-black text-sm font-normal leading-normal">Our website and App may contain links to third-party services, such as social media platforms. We are not responsible for the privacy practices of those third parties, and we encourage you to review their privacy policies.<br /></span><span className="text-black text-sm font-semibold leading-normal"><br />Contact Us<br /></span><span className="text-black text-sm font-normal leading-normal">If you have any questions or concerns about this Privacy Policy, please contact us at:<br />+1 (310) PL8-CHAT<br /> <a href={'mailto:info@pl8chat.com'}>info@pl8chat.com</a></span></div>
     </div>
   );
 }
