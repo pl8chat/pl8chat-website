bd9ef135f7613172e55f514808c72cadc942e247
zachcygan <zachcygan@gmail.com>
2025-10-23 17:48:09 -0600

    minor changes

--- src/components/comingSoon.tsx
+++ src/components/comingSoon.tsx
@@ -3,7 +3,7 @@ import Pl8chatWhiteMobile from '@/svgs/Pl8chatWhiteMobile.svg';
 
 export default function ComingSoon() {
   return (
-    <div className="relative h-screen w-full pb-10 inline-flex flex-col justify-center items-center bg-[url('/assets/images/tempBg.jpg')] bg-cover bg-no-repeat bg-center">
+    <div className="relative h-screen w-full pb-10 inline-flex flex-col justify-center items-center bg-[url('/assets/images/tempBgMobile.jpg')] md:bg-[url('/assets/images/tempBg.jpg')] md:bg-cover bg-[position:74%_center] bg-[length:350%_auto] md:bg-center bg-no-repeat">
       {/* Dark overlay */}
       <div className="absolute inset-0 bg-black/55 w-full" />
 
@@ -18,7 +18,7 @@ export default function ComingSoon() {
       <div className="relative z-10 w-[358px] md:w-[768px] h-[656px] flex flex-col justify-center items-center gap-10">
         <div className="self-stretch flex flex-col justify-center items-center gap-6">
           <div className="self-stretch flex flex-col justify-start items-start gap-5 md:gap-20">
-            <div className="self-stretch text-center justify-start text-white text-4xl md:text-7xl font-medium leading-10 md:leading-[66px]">
+            <div className="self-stretch text-center justify-start text-white text-[39px] md:text-7xl font-medium leading-10 md:leading-[66px]">
               <br />A new line of vehicle<br />communication.
             </div>
             <div className="self-stretch text-center justify-start">
